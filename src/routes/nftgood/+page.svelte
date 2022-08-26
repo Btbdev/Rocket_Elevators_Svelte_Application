e7edb721c8c159051366665cec40cac898b0cf6b
Mathieu Bernier <mathieubernier27@gmail.com>
2022-08-26 13:17:31 -0400

    website finished

--- src/routes/nftgood/+page.svelte
+++ src/routes/nftgood/+page.svelte
@@ -1,10 +1,9 @@
 <script>
-	import { enhance } from '$lib/form';
-	import { scale } from 'svelte/transition';
-	import { flip } from 'svelte/animate';
 	import { onMount } from 'svelte';
 	import { connected, web3, selectedAccount, chainId, chainData } from 'svelte-web3';
 	import { defaultEvmStores } from 'svelte-web3';
+	import { Styles } from 'sveltestrap';
+	import { Button, Tooltip } from 'sveltestrap';
 
 	// to add the connection with Metamask
 	import RocketTokenContract from '../../RocketToken.json';
@@ -52,34 +51,14 @@
 		}
 	}
 
-	// let isActive = false;
 	// to check if the nft smart contract allowance   ${checkAccount}
 	onMount(async function () {
 		const res = await fetch(`https://express-api.codeboxxtest.xyz/NFT/allowance/${checkAccount}`);
 
 		const data = await res.json();
 		if (data === true) {
-			// window.location.replace('/portfolio');
-
-			alert('Success ! Here comes your portfolio !');
 		}
 	});
-
-	// to add a button that will trigger the right redirection for the user, depending on its wallet content
-	// let user = { data: false };
-
-	// function toggle () {
-	// 	user.data = !user.data
-	// }
-
-	//
-	// onMount(async function () {
-	// 	const tokenBlance = await fetch(`https://express-api.codeboxxtest.xyz/ERC20/balance/${checkAccount}`);
-	// 	console.log("the tokenBlance is:", tokenBlance);
-	// 	const data = await tokenBlance.json();
-	// 	console.log("Nb of tokens available:", data)
-
-	// });
 
 	// to buy NFT with Rocket token
 
@@ -91,7 +70,6 @@
 			}
 		);
 		console.log('the mintNft is:', res);
-		// const json = await mintNft.json();
 
 		if (res.status === 200) {
 			window.location.replace('/portfolio');
@@ -103,6 +81,10 @@
 <svelte:head>
 	<title>NFT minting</title>
 	<meta name="description" content="A todo list app" />
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+	/>
 </svelte:head>
 
 <div class="todos">
@@ -111,11 +93,12 @@
 	<div class="todos-wallet">
 		<h1>Your Wallet Address <span class="wallet">{checkAccount}</span></h1>
 
-		<button class="button" on:click={approve}> Connect my wallet </button> <br />
+		<Styles />
+		<Button color="danger" size="lg" on:click={approve}>Connect my wallet</Button> <br />
 
 		<div class="button-buy">
 			{#if isAddressEligible}
-				<button class="button" on:click={doPost}> Buy NFT </button>
+				<Button color="primary" size="lg" on:click={doPost}>Buy NFT</Button>
 			{/if}
 		</div>
 	</div>
@@ -137,113 +120,12 @@
 		font-size: 20px;
 	}
 
-	.new {
-		margin: 0 0 0.5rem 0;
-	}
-
-	input {
-		border: 1px solid transparent;
-	}
-
-	input:focus-visible {
-		box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
-		border: 1px solid #ff3e00 !important;
-		outline: none;
-	}
-
-	.new input {
-		font-size: 28px;
-		width: 100%;
-		padding: 0.5em 1em 0.3em 1em;
-		box-sizing: border-box;
-		background: rgba(255, 255, 255, 0.05);
-		border-radius: 8px;
-		text-align: center;
-	}
-
-	.todo {
-		display: grid;
-		grid-template-columns: 2rem 1fr 2rem;
-		grid-gap: 0.5rem;
-		align-items: center;
-		margin: 0 0 0.5rem 0;
-		padding: 0.5rem;
-		background-color: white;
-		border-radius: 8px;
-		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
-		transform: translate(-1px, -1px);
-		transition: filter 0.2s, transform 0.2s;
-	}
-
-	.done {
-		transform: none;
-		opacity: 0.4;
-		filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.1));
-	}
-
-	form.text {
-		position: relative;
-		display: flex;
-		align-items: center;
-		flex: 1;
-	}
-
-	.todo input {
-		flex: 1;
-		padding: 0.5em 2em 0.5em 0.8em;
-		border-radius: 3px;
-	}
-
-	.todo button {
-		width: 2em;
-		height: 2em;
-		border: none;
-		background-color: transparent;
-		background-position: 50% 50%;
-		background-repeat: no-repeat;
-	}
-
-	button.toggle {
-		border: 1px solid rgba(0, 0, 0, 0.2);
-		border-radius: 50%;
-		box-sizing: border-box;
-		background-size: 1em auto;
-	}
-
-	.done .toggle {
-		background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
-	}
-
-	.delete {
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.5 5V22H19.5V5H4.5Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M10 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M14 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M2 5H22' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M8 5L9.6445 2H14.3885L16 5H8Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
-		opacity: 0.2;
-	}
-
-	.delete:hover,
-	.delete:focus {
-		transition: opacity 0.2s;
-		opacity: 1;
-	}
-
-	.save {
-		position: absolute;
-		right: 0;
-		opacity: 0;
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
-	}
-
-	.todo input:focus + .save,
-	.save:focus {
-		transition: opacity 0.2s;
-		opacity: 1;
-	}
-
-	.button {
+	/* .button {
 		background-color: #fff;
 		height: 50px;
 		width: 100%;
 		font-size: 18px;
-	}
+	} */
 
 	.button-buy {
 		padding-top: 50px;
